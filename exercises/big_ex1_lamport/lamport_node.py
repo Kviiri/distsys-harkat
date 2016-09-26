469d636010bf9ed7af585f8aae16b51802b85dac
Kalle Viiri <kalle.viiri@gmail.com>
2016-09-26 23:07:23 +0300

    more work towards lamport node

--- exercises/big_ex1_lamport/lamport_node.py
+++ exercises/big_ex1_lamport/lamport_node.py
@@ -3,7 +3,9 @@ import random as rng
 
 class Node:
 
-    def __init__(hostname, port, outports):
+    def __init__(hostname, port, outports, id):
+        self.hostname = hostname
+        self.id = id
         self.s = socket.socket(type = socket.SOCK_DGRAM)
         self.s.bind((hostname, port))
         self.o = socket.socket(type = socket.SOCK_DGRAM)
@@ -13,7 +15,8 @@ class Node:
         startReceiving()
 
     
-    def start():
+    def start(self):
+        self.log = open(id + ".log", "a")
         for i in range(1, 100):
             if(rng.randint(0, 1) == 0):
                 localEvent()
@@ -21,12 +24,19 @@ class Node:
                 sendMessage()
 
     def localEvent(self):
-        self.clock += rng.randint(1,5);
+        increment = rng.randint(1,5)
+        self.clock += increment
+        self.log.write("l " + increment)
 
     def sendMessage(self):
-
+        target = outports[rng.randint(0, outports.len)]
+        message = self.id + ":" + str(self.clock)
+        self.o.sendto(message, ((self.hostname, target)))
 
     def startReceiving(self):
         while(self.active):
-            msg = self.s.recv(128)
-            
+            msg = self.s.recv(128).split(":")
+            sender = msg[0]
+            self.clock = max(self.clock, int(msg[1])) + 1
+            self.log.write("r " + sender + " " + int(msg[1]) + " " + self.clock)
+
