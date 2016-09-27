46b81d278bb06575719169668ac14ef438f859c9
Kalle Viiri <kalle.viiri@gmail.com>
2016-09-28 01:50:23 +0300

    more work towards the UDP communication

--- exercises/big_ex1_lamport/lamport_node.py
+++ exercises/big_ex1_lamport/lamport_node.py
@@ -1,8 +1,75 @@
 import socket
 import selectors
+import sys
 import random as rng
 
 select = selectors.DefaultSelector()
+if (len(argv) != 3):
+    raise TypeError('Wrong number of arguments!')
+
+confpath = argv[1]
+confline = int(argv[2])
+with open(confpath, 'r') as conf:
+    conflines = conf.readlines()
+
+inbox = socket.socket(type = socket.SOCK_DGRAM)
+outbox = socket.socket(type = socket.SOCK_DGRAM)
+outports = []
+first = False
+for i in range(0, len(conflines)):
+    line = conflines[i].split()
+    if (i == confline):
+        id = line[0]
+        inbox.bind(("localhost", int(line[1])))
+        if(i == 0):
+            first = True
+    else:
+        outports.append((line[0], int(line[1])))
+
+try:
+    log = open(id + ".log", "a")
+except NameError as e:
+    print "id is not defined: " + e.strerror
+except IOError as e:
+    print "error opening log file: " + e.strerror
+
+
+#now we have the following:
+#outports contains tuples (id, port) for other nodes
+
+#inbox is listening the specified port
+#outbox is unbound - it'll be used for sending messages
+
+#log is the log file, named <id>.log
+
+#first node pings others until everyone's up
+
+if (first):
+    inbox.settimeout(2)
+    readyPorts = []
+    while (len(readyPorts) != len(outports)):
+        for port in outports:
+            if port[1] in readyPorts:
+                continue
+            outbox.sendto("PING:" + str(id), ("localhost", port[1]))
+        try:
+            while True:
+                reply = s.recv(128)
+                readyPorts.append(int(reply))
+        except socket.timeout:
+            #if timeout is exceeded, we simply pass
+            pass
+    #all ports ready - time to start the algorithm
+    for port in readyPorts:
+        outbox.sendTo("START", ("localhost", port))
+
+#others than first node wait for PING to let the first know they're ready
+else:
+    message = inbox.recv(128).split(":")
+    if message[0] == "PING":
+        replyto = outports[message[1]] #where to reply to
+        outbox.sendto(str(id), ("localhost", replyto))
+    
 
 def __init__(hostname, port, outports, id):
     self.hostname = hostname
