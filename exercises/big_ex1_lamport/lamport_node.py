b52bbe740cbba308c55098f44fd903bf5b748e68
Kalle Viiri <kalle.viiri@gmail.com>
2016-10-05 20:04:06 +0300

    finalizing the first project work

--- exercises/big_ex1_lamport/lamport_node.py
+++ exercises/big_ex1_lamport/lamport_node.py
@@ -1,10 +1,9 @@
 import socket
-#import selectors
 import sys
 import random as rng
 import threading
+import time
 
-#select = selectors.DefaultSelector()
 argv = sys.argv
 if (len(argv) != 3):
     raise TypeError('Wrong number of arguments!')
@@ -15,8 +14,6 @@ with open(confpath, 'r') as conf:
     conflines = conf.readlines()
 
 outports = {} 
-#connections = []
-#ownLineFound = False
 first = False
 inbox = socket.socket(type = socket.SOCK_DGRAM)
 outbox = socket.socket(type = socket.SOCK_DGRAM)
@@ -25,53 +22,21 @@ for i in range(0, len(conflines)):
     if (i == confline):
         inbox.bind(("localhost", int(line[1])))
         id = line[0]
-        #ownLineFound = True
-        #every node tries to connect to all nodes after it
-        #first node doesn't listen, last node doesn't connect to outside
-        #if i != 0:
-        #    listener = socket.socket()
         if i == 0:
             first = True
     else:
         outports[line[0]] = int(line[1])
-        #if ownLineFound:
-        #    newSocket = socket.socket()
-        #    connections.append((id, newSocket))
-
-
 
 #now we have the following:
 #outports contains tuples (id, port) for other nodes
-#connections contains OUTGOING connection sockets for nodes (only latter ones)
-#listener is the listening socket
 
 #inbox is listening the specified port
 #outbox is unbound - it'll be used for sending messages
 
-#log is the log file, named <id>.log
-
-#create the connections.
-#each socket except the first begins listening
-
-#if confline != 0:
-#    listener.listen(512)
-#
-#while (len(connections) != len(outports)):
-#    for id in outports.keys():
-#        try:
-#            newconn = socket.socket()
-#            newconn.connect(("localhost"), outports(id))
-#        except socket.error as e:
-#            continue #in case no one's listening yet, just ignore and retry later
-#        connections.append((id, newconn))
-
-print outports
-print str(id)
-
+#clock is initialized
 clock = 0
-print "clock " + str(clock)
 
-
+#first node pings everyone until everyone's replied
 if (first):
     readyPorts = []
     while (len(readyPorts) != len(outports)):
@@ -85,9 +50,8 @@ if (first):
                 pass
         try:
             while True:
-                print "waiting for reply"
                 reply = inbox.recv(128)
-                print "got reply from: " + reply
+                #if we hadn't got replies from this node yet, add them to ready ports
                 if int(reply) not in readyPorts:
                     readyPorts.append(outports[reply])
         except socket.timeout:
@@ -95,18 +59,18 @@ if (first):
             pass
     #all ports ready - time to start the algorithm
     for port in readyPorts:
-        print "sending START!"
+        #send START to all, signifying that the first node is ready
         outbox.sendto("START", ("localhost", port))
 
 #others than first node wait for PING to let the first know they're ready
 else:
     message = inbox.recv(128).split(":")
+    #PING got!
     if message[0] == "PING":
-        print "got ping!"
         replyto = outports[message[1]] #where to reply to
         outbox.sendto(str(id), ("localhost", replyto))
-    print "waiting for start"
     while True:
+        #wait for START
         message = inbox.recv(128)
         if message == 'START':
             inbox.settimeout(2)
@@ -114,15 +78,21 @@ else:
         else:
             print message
 
-
+#next the methods of the Lamport clock algorithm
+#start: starts the algorithm and executes for 100 steps
 def start():
     for i in range(1, 100):
+        #coinflip between local and send event
+        #only local events happen if no other nodes are active
         if(rng.randint(0, 1) == 0) or not outports:
             localEvent()
         else:
             sendMessage()
 
+#localevent: increments counter by 1, 2, 3, 4 or 5
 def localEvent():
+    #short sleep to simulate actually doing something locally
+    time.sleep(0.05)
     increment = rng.randint(1,5)
     global clock
     clockLock.acquire()
@@ -132,11 +102,13 @@ def localEvent():
     print "l " + str(increment)
     printLock.release()
 
+#sendMessage: picks a random node from the remaining ones and sends a message
 def sendMessage():
     global clock
+    #uniformly random selection from ids to send to
     target = rng.choice(outports.keys()) 
     clockLock.acquire()
-    clock += 1
+    clock += 1 #increment clock
     message = id + ":" + str(clock)
     outbox.sendto(message, (("localhost", outports[target])))
     printLock.acquire()
@@ -144,15 +116,18 @@ def sendMessage():
     printLock.release()
     clockLock.release()
 
+#startReceiving: executed in a separate thread
+#receives both main messages and SIGNOFF messages
 def startReceiving():
     while(active):
         try:
             msg = inbox.recv(128).split(":")
         except socket.timeout:
             continue
+        #SIGNOFF messages remove the specified element from outports
+        #format: SIGNOFF:<id>
         if msg[0] == "SIGNOFF":
             del outports[msg[1]]
-            print "DELETED PORT " + msg[1]
             continue
         sender = msg[0]
         global clock
@@ -163,13 +138,21 @@ def startReceiving():
         print "r " + sender + " " + msg[1] + " " + str(clock)
         printLock.release()
 
+
+#fire up the algorithm
+#printLock: locks printing for thread safety
+#clockLock: locks clock for thread safety
 printLock = threading.Lock()
 clockLock = threading.Lock()
+#active: True until 100 events have happened
 active = True
+#start a new thread for receiving messages
 receiverThread = threading.Thread(target = startReceiving)
 receiverThread.start()
+#start the algorithm
 start()
+#after algorithm, this boolean lets receiver thread know it should terminate
 active = False
+#let everyone else know we no longer accept messages
 for port in outports.values():
-    print "SHUT UP " + str(port)
     outbox.sendto("SIGNOFF:" + id, ("localhost", port))
